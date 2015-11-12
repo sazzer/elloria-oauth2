4bc3389141ae0b4273c597b8d772d8b181b5b0ec
Graham Cox <graham@grahamcox.co.uk>
2015-11-12 08:06:48 +0000

    Added ESLint into the mix

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,21 @@
 const gulp = require('gulp');
 const server = require('gulp-develop-server');
+const eslint = require('gulp-eslint');
+
+gulp.task('lint:main', () => {
+    return gulp.src('src/main/**/*.js')
+        .pipe(eslint({
+            configFile: './eslintrc'
+        }))
+        .pipe(eslint.format())
+        .pipe(eslint.failAfterError());
+});
 
 gulp.task('unit-test');
 gulp.task('integration-test');
 
-gulp.task('build', ['unit-test']);
-gulp.task('test', ['unit-test', 'integration-test']);
+gulp.task('build', ['lint:main', 'unit-test']);
+gulp.task('test', ['build', 'unit-test', 'integration-test']);
 
 gulp.task('start', ['build'], () => {
     server.listen({path: './src/main/index.js'});
--- src/main/index.js
+++ src/main/index.js
@@ -1,1 +1,5 @@
-console.log('Hello');
+const lambda = () => {
+    console.log('Hello');
+};
+
+lambda();
