3380ffc80861703e53ba8cb4324ad564cffcb5a3
Graham Cox <graham@grahamcox.co.uk>
2015-11-13 17:05:54 +0000

    Set up mockery and sinon for testing that the correct time is returned

--- src/test/server/routes/debug-spec.js
+++ src/test/server/routes/debug-spec.js
@@ -3,11 +3,31 @@
 const expect = require('chai').expect;
 const express = require('express');
 const request = require('supertest-as-promised');
-const debugRoutes = require('root-require')('src/main/server/routes/debug');
+const mockery = require('mockery');
+const sinon = require('sinon');
+const moment = require('moment-timezone');
 
 describe('Debug Routes', () => {
     let app;
+    let mockMoment;
+    
+    before(() => {
+        mockery.enable({
+            warnOnReplace: false,
+            warnOnUnregistered: false,
+            useCleanCache: true
+        });
+    });
+    
+    after(() => {
+        mockery.disable();
+    });
+    
     beforeEach(() => {
+        mockMoment = sinon.stub().returns(moment('2014-06-01T12:00:00Z'));
+        mockery.registerMock('moment-timezone', mockMoment);
+            
+        const debugRoutes = require('root-require')('src/main/server/routes/debug');
         app = express();
         debugRoutes(app);
     });
@@ -44,6 +64,9 @@ describe('Debug Routes', () => {
         });
         it('Returns a content type of text/plain', () => {
             return response.expect('Content-Type', /text\/plain/);
+        });
+        it('Returns the expected payload', () => {
+            return response.expect('2014-06-01T12:00:00+00:00');
         });
     });
 });
