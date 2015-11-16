055e4f7e58e7a9a67bcba5e05366edfb29696c1a
Graham Cox <graham@grahamcox.co.uk>
2015-11-16 16:08:39 -0500

    Deleted the dummy tests

--- src/test/first-spec.js
+++
@@ -1,9 +0,0 @@
-const expect = require('chai').expect;
-const lambda = require('../main/second').lambda;
-
-describe('Lambda', () => {
-    const value = lambda();
-    it('Should return the correct string', () => {
-        expect(value).to.equal('Hello, World');
-    });
-});
