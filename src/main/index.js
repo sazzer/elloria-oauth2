6fd9c3a6e3533caddf32f1ef09ccd4c0b2acf104
Graham Cox <graham@grahamcox.co.uk>
2015-11-16 21:39:34 -0500

    Exclude the main entrypoint from code coverage

--- src/main/index.js
+++ src/main/index.js
@@ -1,11 +1,14 @@
-const express = require('./server');
-const log = require('./log');
+/* istanbul ignore next */
+(() => {
+    const express = require('./server');
+    const log = require('./log');
 
-const app = express();
+    const app = express();
 
-const server = app.listen(3000, function () {
-    const host = server.address().address;
-    const port = server.address().port;
+    const server = app.listen(3000, function () {
+        const host = server.address().address;
+        const port = server.address().port;
 
-    log.info({host, port}, 'Example app listening at http://%s:%s', host, port);
-});
+        log.info({host, port}, 'Example app listening at http://%s:%s', host, port);
+    });
+})();
