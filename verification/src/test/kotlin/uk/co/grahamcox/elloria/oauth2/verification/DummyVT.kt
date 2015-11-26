74b9c5de37d22087ab1ec91d76562874b441d48b
Graham Cox <graham@grahamcox.co.uk>
2015-11-26 21:08:32 +0000

    Verification module that, if the correct module is enabled, will start up the webapp for the duration of the integration tests

---
+++ verification/src/test/kotlin/uk/co/grahamcox/elloria/oauth2/verification/DummyVT.kt
@@ -0,0 +1,11 @@
+package uk.co.grahamcox.elloria.oauth2.verification
+
+import org.junit.Test
+import java.util.concurrent.TimeUnit
+
+class DummyVT {
+    @Test
+    fun test() {
+        TimeUnit.SECONDS.sleep(10)
+    }
+}
