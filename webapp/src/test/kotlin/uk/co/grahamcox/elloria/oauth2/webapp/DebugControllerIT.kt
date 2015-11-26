7851c683b84da87942b663bfad71c43addfa1b54
Graham Cox <graham@grahamcox.co.uk>
2015-11-26 07:49:06 +0000

    Test the Ping handler

--- webapp/src/test/kotlin/uk/co/grahamcox/elloria/oauth2/webapp/DebugControllerIT.kt
+++ webapp/src/test/kotlin/uk/co/grahamcox/elloria/oauth2/webapp/DebugControllerIT.kt
@@ -1,6 +1,9 @@
 package uk.co.grahamcox.elloria.oauth2.webapp
 
 import org.junit.Test
+import org.springframework.http.MediaType
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.*
 
 /**
  * Integration test for the Debug Controller
@@ -11,6 +14,8 @@ class DebugControllerIT : SpringTestBase() {
      */
     @Test
     fun testPing() {
-
+        perform(get("/api/debug/ping"))
+            .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_PLAIN))
+            .andExpect(content().string("Pong"))
     }
 }
